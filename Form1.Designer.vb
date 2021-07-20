9bd78d26f0f42ae11130d2dd0b5a895fa75e7c9a
Gifty29Walker <matlogadineo@gmail.com>
2021-07-20 14:09:17 +0200

    Edited the form properties and created a setup with a logo added.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -692,13 +692,13 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(804, 565)
+        Me.ClientSize = New System.Drawing.Size(834, 565)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Setup"
+        Me.Text = "Stock control"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
